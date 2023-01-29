1383504b942055641469daca6c5516a4e141d9d9
kingbj0429 <kingbj0429@lunit.io>
2023-01-29 15:24:37 +0900

    Add: crane-claw-machine-game

--- programmers/kakao/lv1/crane-claw-machine-game/kbj.py
+++ programmers/kakao/lv1/crane-claw-machine-game/kbj.py
@@ -1,12 +1,29 @@
+# 문제 이해하기, 접근 방식, 코드 설계, 코드 구현
+# Bio-O(N^2)
 def solution(board, moves):
     answer = 0
-
-    bucket = []
+    N = len(board)
+    bucket = [0]
 
     for move in moves:
-        board[move-1]
+        for i in range(0,N):
+            if board[i][move-1] != 0:
+                obj = board[i][move-1]
+                answer += checkBucket(bucket, obj)
+
+                board[i][move - 1] = 0
+                break
 
     return answer
+
+def checkBucket(bucket, new):
+    if bucket[-1] == new:
+        bucket.pop()
+        return 2
+    else:
+        bucket.append(new)
+        return 0
+
 
 if __name__ == "__main__":
     board = [[0,0,0,0,0],
@@ -16,3 +33,5 @@ if __name__ == "__main__":
              [3,5,1,3,1]]
 
     moves = [1,5,3,5,1,2,1,4]
+
+    print(solution(board, moves))
