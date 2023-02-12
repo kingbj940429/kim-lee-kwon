a40139c0b5041d0c520a7186ec5fb6c889039427
kingbj0429 <kingbj0429@lunit.io>
2023-02-12 15:51:02 +0900

    Add: 1차 캐시

--- programmers/kakao/lv1/신고 결과 받기/kbj.py
+++ programmers/kakao/lv1/신고 결과 받기/kbj.py
@@ -1,21 +1,52 @@
+# 문제이해, 접근방식, 코드설계, 코드구현
+# id_list, report 둘 다 시간복잡도에 관여
 def solution(id_list, report, k):
-    answer = [0] * len(id_list)
+    answer = [0 for _ in range(len(id_list))]
 
-    report = list(set(report))
-    to_repo_list = [[] for _ in range(len(id_list))]
-    k_over_list = [0] * len(id_list)
+    report_count = {key: 0 for key in id_list}
 
-    for val in report:
-        from_repo, to_repo = val.split()
-        idx = id_list.index(from_repo)
-        to_repo_list[idx].append(to_repo)
+    reporting_list = [[] for _ in range(len(id_list))]
 
-        k_over_list[id_list.index(to_repo)] += 1
+    for r in report:
+        reporting, reported = r.split(" ")[0], r.split(" ")[1]
 
-    for i in range(len(to_repo_list)):
-        for j in range(len(to_repo_list[i])):
-            idx = id_list.index(to_repo_list[i][j])
-            if k_over_list[idx] >= k:
+        if reported not in reporting_list[id_list.index(reporting)]:
+            reporting_list[id_list.index(reporting)].append(reported)
+            report_count[reported] += 1
+
+    for i, val in enumerate(reporting_list):
+        for j in val:
+            if report_count[j] >= k:
                 answer[i] += 1
 
     return answer
+
+if __name__=="__main__":
+    id_list = ["muzi", "frodo", "apeach", "neo"]
+    report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+    k = 2
+
+    solution(id_list, report, k)
+
+
+# def solution(id_list, report, k):
+#     answer = [0] * len(id_list)
+#
+#     report = list(set(report))
+#     to_repo_list = [[] for _ in range(len(id_list))]
+#     k_over_list = [0] * len(id_list)
+#
+#     for val in report:
+#         from_repo, to_repo = val.split()
+#         idx = id_list.index(from_repo)
+#         to_repo_list[idx].append(to_repo)
+#
+#         k_over_list[id_list.index(to_repo)] += 1
+#
+#     for i in range(len(to_repo_list)):
+#         for j in range(len(to_repo_list[i])):
+#             idx = id_list.index(to_repo_list[i][j])
+#             if k_over_list[idx] >= k:
+#                 answer[i] += 1
+#
+#     return answer
---
+++ programmers/kakao/lv2/[1차] 캐시/kbj.py
@@ -0,0 +1,38 @@
+# 문제이해, 접근방식, 코드설계, 코드구현
+# 도시 갯수가 10^5 이므로 n^2 일 시 시간초과 가능성 있음
+# Queue 문제
+
+from collections import deque
+
+def solution(cacheSize, cities):
+    if cacheSize == 0:
+        return len(cities) * 5
+
+    answer = 0
+    cache = deque()
+
+    for city in cities:
+        city = city.lower()
+
+        if city not in cache:
+            answer += 5
+
+            if len(cache) < cacheSize:
+                cache.append(city)
+            else:
+                cache.popleft()
+                cache.append(city)
+        elif city in cache:
+            answer += 1
+
+            cache.remove(city)
+            cache.append(city)
+
+    return answer
+
+
+if __name__=="__main__":
+    cacheSize = 3
+    cities = ["Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"]
+
+    print(solution(cacheSize, cities))
