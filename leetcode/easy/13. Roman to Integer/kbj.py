2257d275de280177b620939565c8e4e4be65214a
kingbj0429 <kingbj0429@lunit.io>
2023-05-07 18:42:44 +0900

    Add: Roman in Integer

--- leetcode/easy/13. Roman to Integer/kbj.py
+++ leetcode/easy/13. Roman to Integer/kbj.py
@@ -31,6 +31,35 @@ class Solution:
 
         return answer
 
+    def romanToInt2(self, s: str) -> int:
+        skip = False
+        answer = 0
+
+        roman = {
+            " ": 0,
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        s += " "
+
+        for i, c in enumerate(s):
+            if skip or c == " ":
+                skip = False
+                continue
+            else:
+                if roman[c] < roman[s[i+1]]:
+                    answer += roman[s[i+1]] - roman[c]
+                    skip = True
+                else:
+                    answer += roman[c]
+        return answer
+
 if __name__=="__main__":
     solution = Solution()
     s = "MCMXCIV"
