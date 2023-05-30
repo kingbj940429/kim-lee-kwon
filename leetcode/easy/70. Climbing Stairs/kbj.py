d77d03d663bd568c5748ff3715b3475992a86d6a
kingbj0429 <kingbj0429@lunit.io>
2023-05-30 21:14:12 +0900

    Update: climbing stairs

--- leetcode/easy/70. Climbing Stairs/kbj.py
+++ leetcode/easy/70. Climbing Stairs/kbj.py
@@ -13,3 +13,16 @@ class Solution:
             memo[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
 
         return memo[n]
+
+#---------------
+from collections import defaultdict
+
+memo = defaultdict(int)
+memo = {1: 1, 2: 2}
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        for i in range(3, n+1):
+            memo[i] = memo[i-1] + memo[i-2]
+
+        return memo[n]
