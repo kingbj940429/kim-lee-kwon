de64638be511a4e680ab245cb4888a7fb8258c5d
kwondongwook <kwondongwook1@gmail.com>
2023-07-24 22:37:00 +0900

    Update: 3

--- leetcode/medium/3. Longest Substring Without Repeating Characters/kdw.py
+++ leetcode/medium/3. Longest Substring Without Repeating Characters/kdw.py
@@ -1,4 +1,5 @@
 class Solution:
+    # using set
     def lengthOfLongestSubstring(self, s: str) -> int:
         max_length = left = right = 0
         window = set()
@@ -8,9 +9,22 @@ class Solution:
             if s[right] not in window:
                 window.add(s[right])
                 right += 1
-                max_length = max(max_length, len(window))
+                max_length = max(max_length, right - left)
             else:
                 window.remove(s[left])
                 left += 1
+
+        return max_length
+
+    # using dictionary
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        max_length = start = 0
+        visited = {}
+
+        for i, char in enumerate(s):
+            if char in visited:
+                start = max(start, visited[char] + 1)
+            visited[char] = i
+            max_length = max(max_length, i - start + 1)
 
         return max_length
